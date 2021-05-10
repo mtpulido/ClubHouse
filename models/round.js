b516c3cb13ce5076968d04edfa8555b6d647361c
Michael Pulido <michaelpulido@Michaels-MacBook-Pro.local>
2021-05-10 11:41:44 -0400

    user and round rough schema's created

--- models/round.js
+++ models/round.js
@@ -3,7 +3,17 @@ const Schema = mongoose.Schema
 
 const Round = new Schema(
   {
-
+    course: { type: String, required: true },
+    par: { type: Number, required: true },
+    holes: { type: Number, required: true },
+    score: { type: Number, required: true },
+    fairwaysHit: { type: Number, required: true },
+    possibleFairways: { type: Number, required: true },
+    putts: { type: Number, required: true },
+    greens: { type: Number, required: true },
+    upAndDowns: { type: Number, required: true },
+    possibleUpAndDowns: { type: Number, required: true },
+    userId: {type: Schema.Types.ObjectId, ref: 'users'}
   },
   { timestamps: true }
 )
--- models/user.js
+++ models/user.js
@@ -3,7 +3,11 @@ const Schema = mongoose.Schema
 
 const User = new Schema(
   {
-
+    email: { type: String, required: true },
+    password_digest: { type: String, required: true },
+    displayName: { type: String, required: true },
+    avatar: { data: Buffer, contentType: String, required: false },
+    rounds: [{ type: Schema.Types.ObjectId, ref: 'rounds' }]
   },
   { timestamps: true }
 )
