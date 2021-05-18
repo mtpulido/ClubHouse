fede87390d609ee4f7b9efb02cef0259ffc068e7
Michael Pulido <michaelpulido@Michaels-MacBook-Pro.local>
2021-05-18 10:18:37 -0400

    material UI error handling incorporated to make it look better, error messages tweaked slightly

--- client/src/screens/signUp/SignUp.jsx
+++ client/src/screens/signUp/SignUp.jsx
@@ -9,16 +9,16 @@ const useStyles = makeStyles({
   root: {
     "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
       borderColor: "#E4E4E4",
-      opacity: 0.4
+      opacity: 0.4,
     },
     "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
       borderColor: "#4CAF50",
-      opacity: 1
+      opacity: 1,
     },
     "& .MuiButton-label": {
       color: "white",
-      fontSize: "16px"
-    }
+      fontSize: "16px",
+    },
   },
 });
 
@@ -32,93 +32,99 @@ const SignUp = (props) => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [passwordError, setPasswordError] = useState("");
 
-  const handleChangeSignUp = (e) => {
-    const { name, value } = e.target;
+  const handleChangeSignUp = (event) => {
+    const { name, value } = event.target;
     setSignUpCredentials((prevState) => ({
       ...prevState,
       [name]: value,
     }));
   };
 
+  const handleSubmit = (event) => {
+    setPasswordError("");
+    props.setCredentialsError([]);
+    if (confirmPassword === signUpCredentials.password) {
+      event.preventDefault();
+      props.handleSignUp(signUpCredentials);
+    }
+    if (confirmPassword !== signUpCredentials.password) {
+      event.preventDefault();
+      setPasswordError("Passwords do not match");
+      props.handleSignUp(signUpCredentials);
+    }
+  };
+
   return (
     <div className="screen-container">
-      <form
-        className="auth-form"
-        onSubmit={(e) => {
-          setPasswordError("");
-          props.setCredentialsError([])
-          if (confirmPassword === signUpCredentials.password) {
-            e.preventDefault();
-            props.handleSignUp(signUpCredentials);
-          }
-          if (confirmPassword !== signUpCredentials.password) {
-            e.preventDefault();
-            setPasswordError("Passwords do not match");
-          }
-        }}
-      >
+      <form className="auth-form" onSubmit={handleSubmit}>
         <div className="auth-form-label">Create Your Account</div>
-  
-          <TextField
-            id="outlined-basic"
-            label="Display Name"
-            id="display name"
-            variant="outlined"
-            placeholder="Mike"
-            name="displayName"
-            value={signUpCredentials.displayName}
-            className={classes.root}
-            onChange={handleChangeSignUp}
-            autoComplete='off'
-          />
-          <TextField
-            id="outlined-basic"
-            id="email"
-            label="Email"
-            variant="outlined"
-            placeholder="example@email.com"
-            name="email"
-            className={classes.root}
-            value={signUpCredentials.email}
-            onChange={handleChangeSignUp}
-            autoComplete='off'
-          />
-          <TextField
-            id="outlined-basic"
-            id="password"
-            label="Password"
-            variant="outlined"
-            type="password"
-            placeholder="password"
-            name="password"
-            className={classes.root}
-            value={signUpCredentials.password}
-            onChange={handleChangeSignUp}
-          />
-          <TextField
-            id="outlined-basic"
-            id="confirm password"
-            label="confirm password"
-            variant="outlined"
-            type="password"
-            placeholder="password"
-            name="confirmPassword"
-            className={classes.root}
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
 
-          {passwordError ? (
-            <div className="auth-errors">{passwordError}</div>
-          ) : null}
+        <TextField
+          id="outlined-basic"
+          label="Display Name"
+          id="display name"
+          variant="outlined"
+          placeholder="Mike"
+          name="displayName"
+          value={signUpCredentials.displayName}
+          className={classes.root}
+          onChange={handleChangeSignUp}
+          autoComplete="off"
+          helperText={props.credentialsError[0]}
+          error={(props.credentialsError[0]) ? true : false}
+        />
+        <TextField
+          id="outlined-basic"
+          id="email"
+          label="Email"
+          variant="outlined"
+          placeholder="example@email.com"
+          name="email"
+          className={classes.root}
+          value={signUpCredentials.email}
+          onChange={handleChangeSignUp}
+          autoComplete="off"
+          helperText={props.credentialsError[1]}
+          error={(props.credentialsError[1]) ? true : false}
+        />
+        <TextField
+          id="outlined-basic"
+          id="password"
+          label="Password"
+          variant="outlined"
+          type="password"
+          placeholder="password"
+          name="password"
+          className={classes.root}
+          value={signUpCredentials.password}
+          onChange={handleChangeSignUp}
+          helperText={props.credentialsError[2]}
+          error={(props.credentialsError[2]) ? true : false}
+        />
+        <TextField
+          id="outlined-basic"
+          id="confirm password"
+          label="confirm password"
+          variant="outlined"
+          type="password"
+          placeholder="password"
+          name="confirmPassword"
+          className={classes.root}
+          value={confirmPassword}
+          onChange={(event) => setConfirmPassword(event.target.value)}
+          helperText={passwordError ? "-Passwords do not match" : ""}
+          error={(passwordError) ? true : false}
+        />
 
-          {props.credentialsError?.map((error) => (
-            <div className="auth-errors">{error}</div>
-          ))}
-
-          <Button variant="contained" color="primary" type="submit" size="large" className={classes.root}>
-            Sign Up
-          </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          size="large"
+          className={classes.root}
+        >
+          Sign Up
+        </Button>
       </form>
     </div>
   );
