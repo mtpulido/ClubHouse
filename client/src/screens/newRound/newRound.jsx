c3766ff13b37bdfa70a5e93f75c04f54c6825b4b
Michael Pulido <michaelpulido@Michaels-MacBook-Pro.local>
2021-06-07 13:53:16 -0400

    uploads folder added again

--- client/src/screens/newRound/newRound.jsx
+++ client/src/screens/newRound/newRound.jsx
@@ -7,7 +7,7 @@ import StepLabel from "@material-ui/core/StepLabel";
 import StepContent from "@material-ui/core/StepContent";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import "./NewRound.css";
+import "./newRound.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
